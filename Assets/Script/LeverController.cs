4c95190985ae7f56d4c50e4e44e0f274be2240b8
Victor Roz <victor.roz@protonmail.com>
2020-10-02 14:38:49 +0200

    lever is working
    my lever is opening the door & it is animated too

--- Assets/Script/LeverController.cs
+++ Assets/Script/LeverController.cs
@@ -8,14 +8,38 @@ public class LeverController : MonoBehaviour
 
     [SerializeField]
     GameObject Text;
+
+    [SerializeField]
+    Animator MyAnimator;
+
+    [SerializeField]
+    Animator MyAnimatorLever;
+
+    [SerializeField]
+    GameObject Door;
+
+    [SerializeField]
+    bool doorStatus = false;
     private void OnTriggerEnter(Collider other)
     {
         Text.SetActive(true);
+
         Debug.Log("Win");
     }
 
     private void OnTriggerExit(Collider other)
     {
         Text.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log("OpenTheDoor");
+            doorStatus = true;
+            MyAnimator.enabled = true;
+            MyAnimatorLever.enabled = true;
+        }
     }
 }
